87f0458af23f6e855d366ab47e641c0caaf90e39
ak-matsu <sandbox.matsu@gmail.com>
2021-02-13 22:32:57 +0900

    ユーザー、ツイートが紐付いていないとコメントが保存できないテストコードを記述

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -10,4 +10,16 @@ RSpec.describe Comment, type: :model do
     @comment.valid?
     expect(@comment.errors.full_messages).to include("Text can't be blank")
   end
+
+  it 'ユーザーが紐付いていないとコメントは保存できない' do
+    @comment.user = nil #nilは存在しないという意味　つまり紐付いていない
+    @comment.valid?
+    expect(@comment.errors.full_messages).to include("User must exist")
+   end
+ 
+   it 'ツイートが紐付いていないとコメントは保存できない' do
+    @comment.tweet = nil #nilは存在しないという意味　つまり紐付いていない
+    @comment.valid?
+    expect(@comment.errors.full_messages).to include("Tweet must exist")
+   end
 end
